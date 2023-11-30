bc6b4cdbe08b2fb748fe3566d282144dadc48718
Mohamed zouine <mohamedzouine827@gmail.com>
2023-11-30 12:59:56 +0100

    python

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     if (length - 1) == 0:
         print("0 arguments.")
     elif (length - 1) == 1:
-        print("{} arguments:".format(length - 1))
+        print("{} argument:".format(length - 1))
         for i, arg in enumerate(argv[1:], start=1):
             print("{}: {}".format(i, arg))
     elif (length - 1) > 0:
