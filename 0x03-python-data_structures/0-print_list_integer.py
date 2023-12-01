248a1c19c0c0230c912ffbcc1aee7a80bafb077f
Mohamed zouine <mohamedzouine827@gmail.com>
2023-12-01 12:18:29 +0100

    python

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    def print_list_integer(list):
-        for i in list:
-            print("{}".format(i), end="\n")
+def print_list_integer(list):
+    for i in list:
+        print("{}".format(i), end="\n")
--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    def element_at(my_list, idx):
-        lenghth = len(my_list)
-        if idx > (lenghth - 1) or idx < 0:
-            return None
-        else:
-            return my_list[idx]
+def element_at(my_list, idx):
+    lenghth = len(my_list)
+    if idx > (lenghth - 1) or idx < 0:
+        return None
+    else:
+        return my_list[idx]
