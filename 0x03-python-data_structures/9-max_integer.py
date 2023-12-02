76fa3abb871ddbc07cd181285136c06bfb61a975
Mohamed zouine <mohamedzouine827@gmail.com>
2023-12-02 23:31:42 +0100

    python

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     Max = 0
-    for item in my_list:
-        if item > Max:
-            Max = item
+    if isinstance(my_list, list):
+        for item in my_list:
+            if item > Max:
+                Max = item
     return Max
