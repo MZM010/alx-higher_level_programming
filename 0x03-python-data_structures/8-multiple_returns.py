9b83a76a65d8888bf3e3a71128336ccdfdcaa1c1
Mohamed zouine <mohamedzouine827@gmail.com>
2023-12-02 23:14:56 +0100

    python

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,4 +3,6 @@ def multiple_returns(sentence):
     if isinstance(sentence, str):
         length = len(sentence)
         first = sentence[0]
+        if not sentence:
+            first = None
         return length, first
