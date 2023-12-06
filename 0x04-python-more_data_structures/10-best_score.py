f9338e05705a4f9fb72a83f183589f5f69539054
Mohamed zouine <mohamedzouine827@gmail.com>
2023-12-07 00:16:35 +0100

    python

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    if isinstance(a_dictionary, str) or a_dictionary is None:
+        return None
     Max = 0
     for i in range(len(a_dictionary)):
         for key, value in a_dictionary.items():
