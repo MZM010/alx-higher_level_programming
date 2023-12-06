c97a844625699fe340a0f355501169daa6ec527a
Mohamed zouine <mohamedzouine827@gmail.com>
2023-12-06 18:28:27 +0100

    python

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    if a_dictionary is None or not isinstance(a_dictionary, dict):
-        return {}
-    for key, value in a_dictionary.items():
-        a_dictionary[key] = value * 2
-    return a_dictionary
+    new_dictionary = a_dictionary.copy()
+    for key, value in new_dictionary.items():
+        new_dictionary[key] = value * 2
+    return new_dictionary
