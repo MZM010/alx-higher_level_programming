5c9f6101f3e65cfcc55e82cd2443dcc813501b3c
Mohamed zouine <mohamedzouine827@gmail.com>
2023-12-18 21:49:01 +0100

    ls

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -10,7 +10,7 @@ def list_division(my_list_1, my_list_2, list_length):
                 division_result = 0
             except IndexError:
                 print("out of range")
-
+                division_result = 0
                 pass
             except TypeError:
                 print("wrong type")
