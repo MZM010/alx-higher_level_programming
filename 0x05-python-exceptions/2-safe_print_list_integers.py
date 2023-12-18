9de93eb34e3645f3a302a6a141aff42de7c28904
Mohamed zouine <mohamedzouine827@gmail.com>
2023-12-18 21:04:26 +0100

    fix

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -11,8 +11,6 @@ def safe_print_list_integers(my_list=[], x=0):
             except TypeError:
                 pass
             i += 1
-    except IndexError:
-        pass
     except ValueError:
         pass
     print()
