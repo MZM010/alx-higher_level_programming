b128316530abeaeb390153778c5e4ee31fe10e4e
Mohamed zouine <mohamedzouine827@gmail.com>
2023-12-18 21:09:39 +0100

    s

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        print("Inside result: {:d}".format(float(a / b)))
+        print("Inside result: {}".format((a / b)))
     except ZeroDivisionError:
+        print("Inside result: {}".format(None))
         return None
     return float(a / b)
