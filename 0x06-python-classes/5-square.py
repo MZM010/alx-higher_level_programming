cdb7d1ab1d17c8e2947e8d6e12c47785eb06a05f
Mohamed zouine <mohamedzouine827@gmail.com>
2023-12-19 20:09:26 +0100

    square

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -21,12 +21,12 @@ class Square:
 
     def area(self):
         return self.__size * self.__size
-    
+
     def my_print(self):
         if self.__size == 0:
             print("")
         else:
             for i in range(self.__size):
                 for j in range(self.__size):
-                    print("#", end= "")
+                    print("#", end="")
                 print("")
